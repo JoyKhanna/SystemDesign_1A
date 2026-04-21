fc94a19d24039d62d78db59c2193c013b3274751
JoyKhanna <88222735+JoyKhanna@users.noreply.github.com>
2026-04-21 10:33:37 +0530

    Delete Flipkart Clone directory

--- Flipkart Clone/backend/routes/orders.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { placeOrder, getOrders, getOrderById } = require('../controllers/orderController');
-
-router.post('/', placeOrder);
-router.get('/', getOrders);
-router.get('/:id', getOrderById);
-
-module.exports = router;
